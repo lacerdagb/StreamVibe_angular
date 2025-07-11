d23b92054f408d8498bf39644e83e0ff48e29272
Gabriel Lacerda <gblacerda15@gmail.com>
2025-07-10 21:39:42 -0300

    atualização

--- src/app/componentes/filmes/filmes.ts
+++ src/app/componentes/filmes/filmes.ts
@@ -7,20 +7,17 @@ import { Component } from '@angular/core';
   styleUrl: './filmes.css'
 })
 export class Filmes {
+  alterarFavorito() {
+    this.filme1.favorito = !this.filme1.favorito
+  }
+
 
   filme1 = {
     nome: "Ataque de tubarão de 3 cabeças",
     imagem: "/tubarao.jpg",
-    sinopse: "Filme de um tubarão de 3 cabeças "
-  }
-  filme2 = {
-    nome: "Tropa de Elite",
-    imagem: "/TropaDeElite.jpg",
-    sinopse: "Mostra como a polícia combate ao crime "
-  }
-  filme3 = {
-    nome: "2012",
-    imagem: "/2012.jpg",
-    sinopse: "Apocalipse do mundo em 2012. "
+    sinopse: "Filme de um tubarão irado que possui 3 cabeças mutantes ",
+    favorito: false
   }
 }
+
+
